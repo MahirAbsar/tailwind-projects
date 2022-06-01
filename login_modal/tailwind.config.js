c91faae6f33018b3943dbff78cc4526c2ec26abb
Mahir Absar <mahir.mohsin@northsouth.edu>
2022-06-01 17:09:50 +0600

    login-modal project started

---
+++ login_modal/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      mono: ['Rokkitt', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
